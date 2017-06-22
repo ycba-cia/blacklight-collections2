818704caedd0a3569acc79d3a8b64e68c1b2fb9b
Eric James <eric.james@yale.edu>
2017-06-22 12:16:41 -0400

    add basic facets

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -71,13 +71,24 @@ class CatalogController < ApplicationController
     #  (useful when user clicks "more" on a large facet and wants to navigate alphabetically across a large set of results)
     # :index_range can be an array or range of prefixes that will be used to create the navigation (note: It is case sensitive when searching values)
 
-    #config.add_facet_field 'format', label: 'Format'
-    #config.add_facet_field 'pub_date', label: 'Publication Year', single: true
-    config.add_facet_field 'subject_topic_facet', label: 'Topic', limit: 20, index_range: 'A'..'Z'
-    config.add_facet_field 'language_facet', label: 'Language', limit: true
-    config.add_facet_field 'lc_1letter_facet', label: 'Call Number'
-    config.add_facet_field 'subject_geo_facet', label: 'Region'
-    config.add_facet_field 'subject_era_facet', label: 'Era'
+    config.add_facet_field 'resource_facet', :label => 'Online Access'
+    config.add_facet_field 'publishDate_ss', :label => 'Publication Year', single: true
+    config.add_facet_field 'collection_facet', :label => 'Collection', :limit => 20
+    config.add_facet_field 'language_facet', :label => 'Language', :limit => true
+    config.add_facet_field 'lc_1letter_facet', :label => 'Call Number'
+    #config.add_facet_field 'geographic_facet', :label => 'Region'
+    config.add_facet_field 'auth_author_display_facet', :label => 'Creator'
+    config.add_facet_field 'author_gender_ss', :label => 'Creator Gender'
+    config.add_facet_field 'title_collective_ss', :label => 'Collective Title'
+    config.add_facet_field 'era_facet', :label => 'Period'
+    config.add_facet_field 'genre_name_facet', :label => 'Genre'
+    config.add_facet_field 'object_name_ss', :label => 'Work Type'
+    config.add_facet_field 'auth_format_ss', :label => 'Medium'
+    config.add_facet_field 'topic_facet', :label => 'Subject Terms'
+    config.add_facet_field 'geographic_facet', :label => 'Place Represented'
+    config.add_facet_field 'topic_frameQuality_facet', :label => 'Frame Quality'
+    config.add_facet_field 'topic_frameStyle_facet', :label => 'Frame Style'
+    config.add_facet_field 'credit_line_facet', :label => 'Credit Line'
 
     #config.add_facet_field 'example_pivot_field', label: 'Pivot Field', :pivot => ['format', 'language_facet']
 
