a859a23faaa3ad875532e78601e7bdc82756c086
Eric James <eric.james@yale.edu>
2018-12-28 16:27:51 -0500

    added footer bar using templating

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,13 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_action :set_fabrique
+  def set_fabrique
+    #set footer_bar variables
+    @footer_bar_links = Array.new
+    @footer_bar_links.push({:href=>"?", :title=>"Privacy & cookies"})
+    @footer_bar_links.push({:href=>"?", :title=>"Terms of use"})
+    @footer_bar_links.push({:href=>"?", :title=>"Colofon"})
+  end
 end
