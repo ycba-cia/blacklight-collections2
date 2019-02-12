f3a6ffc34dac569ba9bc8aba1db972e16655fdbd
Eric James <eric.james@yale.edu>
2019-02-12 17:30:16 -0500

    footer info and mass scss

---
+++ scripts/get_fabrique.sh
@@ -0,0 +1,22 @@
+cp /Users/ermadmix/Documents/github_clones/ycba/website/assets/stylesheets/definitions/_mixins.scss /Users/ermadmix/Documents/RubymineProjects/blacklight-collections2/app/assets/stylesheets/fabrique/_mixins.scss
+cp /Users/ermadmix/Documents/github_clones/ycba/website/assets/stylesheets/grid/_settings.scss /Users/ermadmix/Documents/RubymineProjects/blacklight-collections2/app/assets/stylesheets/fabrique/_settings.scss
+cp /Users/ermadmix/Documents/github_clones/ycba/website/assets/stylesheets/definitions/_styles.scss /Users/ermadmix/Documents/RubymineProjects/blacklight-collections2/app/assets/stylesheets/fabrique/_styles.scss
+cp /Users/ermadmix/Documents/github_clones/ycba/website/assets/stylesheets/definitions/_typography.scss /Users/ermadmix/Documents/RubymineProjects/blacklight-collections2/app/assets/stylesheets/fabrique/_typography.scss
+cp /Users/ermadmix/Documents/github_clones/ycba/website/assets/stylesheets/definitions/_variables.scss /Users/ermadmix/Documents/RubymineProjects/blacklight-collections2/app/assets/stylesheets/fabrique/_variables.scss
+cp /Users/ermadmix/Documents/github_clones/ycba/website/components/atoms/button/button.scss /Users/ermadmix/Documents/RubymineProjects/blacklight-collections2/app/assets/stylesheets/fabrique/button.scss
+cp /Users/ermadmix/Documents/github_clones/ycba/website/components/structures/footer-bar/footer-bar.scss /Users/ermadmix/Documents/RubymineProjects/blacklight-collections2/app/assets/stylesheets/fabrique/footer-bar.scss
+cp /Users/ermadmix/Documents/github_clones/ycba/website/components/atoms/link/link.scss /Users/ermadmix/Documents/RubymineProjects/blacklight-collections2/app/assets/stylesheets/fabrique/link.scss
+cp /Users/ermadmix/Documents/github_clones/ycba/website/components/atoms/logo/logo.scss /Users/ermadmix/Documents/RubymineProjects/blacklight-collections2/app/assets/stylesheets/fabrique/logo.scss
+cp /Users/ermadmix/Documents/github_clones/ycba/website/components/structures/menu-bar/menu-bar.scss /Users/ermadmix/Documents/RubymineProjects/blacklight-collections2/app/assets/stylesheets/fabrique/menu-bar.scss
+cp /Users/ermadmix/Documents/github_clones/ycba/website/vendor/stylesheets/fabrique/mixins/_disable-user-select.scss /Users/ermadmix/Documents/RubymineProjects/blacklight-collections2/app/assets/stylesheets/fabrique/_disable-user-select.scss
+cp /Users/ermadmix/Documents/github_clones/ycba/website/vendor/stylesheets/fabrique/mixins/_override-tap-highlight-color.scss /Users/ermadmix/Documents/RubymineProjects/blacklight-collections2/app/assets/stylesheets/fabrique/_override-tap-highlight-color.scss
+cp /Users/ermadmix/Documents/github_clones/ycba/website/vendor/stylesheets/fabrique/mixins/_placeholder.scss /Users/ermadmix/Documents/RubymineProjects/blacklight-collections2/app/assets/stylesheets/fabrique/_placeholder.scss
+
+cp /Users/ermadmix/Documents/github_clones/ycba/website/components/structures/footer/footer.scss /Users/ermadmix/Documents/RubymineProjects/blacklight-collections2/app/assets/stylesheets/fabrique/footer.scss
+cp /Users/ermadmix/Documents/github_clones/ycba/website/components/molecules/link-list/link-list.scss /Users/ermadmix/Documents/RubymineProjects/blacklight-collections2/app/assets/stylesheets/fabrique/link-list.scss
+cp /Users/ermadmix/Documents/github_clones/ycba/website/components/atoms/definition-list/definition-list.scss /Users/ermadmix/Documents/RubymineProjects/blacklight-collections2/app/assets/stylesheets/fabrique/definition-list.scss
+
+
+
+
+
