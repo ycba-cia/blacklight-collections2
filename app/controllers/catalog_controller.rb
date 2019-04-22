2232821c8d8c09112e27198d15a9058d212b9dc1
Eric James <eric.james@yale.edu>
2019-04-22 16:35:54 -0400

    date asc rather than desc

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -325,7 +325,7 @@ class CatalogController < ApplicationController
     #config.add_sort_field 'title_sort asc, pub_date_sort desc', label: 'title'
     config.add_sort_field 'collection_sort_s asc, score desc', label: 'relevance'
     config.add_sort_field 'title_s asc, score desc', label: 'title'
-    config.add_sort_field 'earliestDate_i desc, score desc', label: 'date'
+    config.add_sort_field 'earliestDate_i asc, score desc', label: 'date'
 
     # If there are more than this many search results, no spelling ("did you
     # mean") suggestion is offered.
