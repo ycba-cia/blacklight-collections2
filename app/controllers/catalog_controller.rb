a58c66b809896c99e469589ea56b15579ac36532
Eric James <eric.james@yale.edu>
2019-05-03 16:31:02 -0400

    #130, #131, reorder masonry results metadata, use short title

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -60,11 +60,11 @@ class CatalogController < ApplicationController
     # }
 
     # solr field configuration for search results/index views
-    config.index.title_field = 'title_txt'
+    config.index.title_field = 'title_ss'
     config.index.display_type_field = 'recordtype_ss'
 
     # solr field configuration for document/show views
-    #config.show.title_field = 'title_txt'
+    #config.show.title_field = 'title_ss'
     config.show.display_type_field = 'recordtype_ss'
 
     # solr fields that will be treated as facets by the blacklight application
