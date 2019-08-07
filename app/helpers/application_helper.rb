68d2452d4a47c85fbfdc571b0457b0e0a2216c50
Eric James <eric.james@yale.edu>
2019-08-07 11:39:32 -0400

    accessibility - use auth/title in search results thumbnail alt field

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -170,7 +170,9 @@ module ApplicationHelper
     url ||= path_to_image('empty_square2.png')
     #square = path_to_image('square.png')
     error_img = path_to_image('empty_square2.png')
-    image_tag url, alt: 'cover image', onerror: "this.src='#{error_img}';"
+    author = document['auth_author_display_ss'][0] ? document['auth_author_display_ss'][0] : ""
+    title_short = document['title_short_ss'][0] ? document['title_short_ss'][0] : ""
+    image_tag url, alt: "#{author} #{title_short}", onerror: "this.src='#{error_img}';"
   end
 
   def doc_thumbnail(document)
