303e8344c5ee00d096bd9319a1c68d3932c953ab
Eric James <eric.james@yale.edu>
2020-01-14 11:45:13 -0500

    carousel images, and fix item page header

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -473,7 +473,7 @@ module ApplicationHelper
     header.push(content_tag(tag, document["title_short_ss"][0], style: "font-weight: bold; font-size: #{fontsize}")) if document["title_short_ss"]
     header.push(content_tag(tag, document["publishDate_ss"][0], style: "font-size: #{fontsize}")) if document["publishDate_ss"]
 
-    fullheader = header.join("&nbsp;").html_safe
+    fullheader = header.join(", ").html_safe
     content_tag("div", fullheader, style:"text-align:center", itemprop: "name")
   end
 end
