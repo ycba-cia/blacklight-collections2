dad75e34f89e51d0ce92e654c50a99dceedf0213
Eric James <eric.james@yale.edu>
2020-03-10 14:58:45 -0400

    lido_to_lux WIP2

--- scripts/lux/lido_to_lux.rb
+++ scripts/lux/lido_to_lux.rb
@@ -15,17 +15,19 @@ REXML::XPath.each(xml, '//lido:workID[@lido:type="inventory number"]') { |x|
   a.push(x.text)
 }
 #puts "identifier_external: #{a.inspect}"
-solrjson["identifier_external"] = a
+solrjson["identifier_external"] = a #NOTE: required
 
 a = Array.new
 REXML::XPath.each(xml, '//lido:descriptiveMetadata/lido:eventWrap/lido:eventSet/lido:event/lido:eventID[@lido:type="TMS"][../lido:eventType/lido:term/text() = "production"]') { |x|
   a.push("tms:#{x.text}")
 }
 #puts "identifier_internal: #{a.inspect}"
-solrjson["identifier_internal"] = a
+solrjson["identifier_internal"] = a #NOTE: required
 
 a = Array.new
+i = 0
 REXML::XPath.each(xml, '//lido:event[lido:eventType/lido:term="production"]/lido:eventActor') { |x|
+  i = i + 1
   a1 = Array.new
   x.elements.each('lido:actorInRole/lido:actor/lido:nameActorSet/lido:appellationValue[@lido:pref="preferred"]') { |x2|
     a1.push(x2.text)
@@ -35,10 +37,58 @@ REXML::XPath.each(xml, '//lido:event[lido:eventType/lido:term="production"]/lido
     #puts "url:#{x2.text}"
     a2.push(x2.text)
   }
-  #TODO - rest on agent children here
-  a.push({"agent" => a1,"agent_identifier_URI" => a2})
+
+  a3 = Array.new
+  x.elements.each('lido:actorInRole/lido:roleActor[lido:conceptID/@lido:type="Object related role"]/lido:term') { |x2|
+    a3.push(x2.text)
+  }
+  a4 = Array.new
+  x.elements.each('lido:displayActorInRole') { |x2|
+    a4.push(x2.text)
+  }
+
+  h = Hash.new
+  h["agent"] = a1 if a1.length > 0
+  h["agent_identifier_URI"] = a2 if a2.length > 0
+  h["agent_role_URI"] = a3 if a3.length > 0
+  h["agent_display"] = a4 if a4.length > 0
+  h["agent_sort"] = i
+  #a.push({"agent" => a1,"agent_identifier_URI" => a2},"agent_role_URI" => a3)
+  a.push(h) if h.length > 0
+
 }
-solrjson["agent"] = a
+solrjson["agent"] = a if a.length > 0
+
+a = Array.new
+REXML::XPath.each(xml, '//lido:titleSet[@lido:type="Repository title"]/lido:appellationValue[@lido:pref="preferred"]') { |x|
+  a.push(x.text)
+}
+
+solrjson["title"] = a if a.length > 0
+
+a = Array.new
+REXML::XPath.each(xml, '//lido:objectIdentificationWrap/lido:displayStateEditionWrap/displayState|displayEdition') { |x|
+  a.push(x.text)
+}
+solrjson["edition"] = a if a.length > 0
+
+a = Array.new
+REXML::XPath.each(xml, '//lido:eventWrap/lido:eventSet/lido:event/lido:eventMaterialsTech/lido:materialsTech/lido:termMaterialsTech/lido:term') { |x|
+  a.push(x.text)
+}
+solrjson["materials"] = a if a.length > 0
+
+a = Array.new
+REXML::XPath.each(xml, '//lido:event[lido:eventType/lido:term="production"]/lido:culture/lido:term') { |x|
+  a.push(x.text)
+}
+solrjson["culture"] = a if a.length > 0
+
+a = Array.new
+REXML::XPath.each(xml, '//lido:descriptiveMetadata/@xml:lang') { |x|
+  a.push(x)
+}
+solrjson["language_of_cataloging"] = a if a.length > 0
 
 solrjson = JSON.pretty_generate(solrjson)
 puts solrjson
