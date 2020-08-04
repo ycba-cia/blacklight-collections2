bc5c483e54554aeb10b7f9f773fd275c9e38e370
Eric James <eric.james@yale.edu>
2020-08-04 10:01:38 -0400

    accessibility #6,7,8 - carousel captions, indicators, controls

--- app/assets/javascripts/ycba_custom.js
+++ app/assets/javascripts/ycba_custom.js
@@ -43,4 +43,19 @@ $(document).on("turbolinks:load",function() {
     //set h1 with title from head
     $("h1").text($("head title").text());
 
+    var $item = $('.item');
+    var $numberofSlides = $('.item').length;
+    var $currentSlide = Math.floor((Math.random() * $numberofSlides));
+
+    $('.carousel-indicators li').each(function(){
+        var $slideValue = $(this).attr('data-slide-to');
+        if($currentSlide == $slideValue) {
+            $(this).addClass('active');
+            $item.eq($slideValue).addClass('active');
+        } else {
+            $(this).removeClass('active');
+            $item.eq($slideValue).removeClass('active');
+        }
+    });
+
 });
