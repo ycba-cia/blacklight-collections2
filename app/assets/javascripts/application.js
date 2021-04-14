ca79dfa256cffc1ef53b19de7a1a66526b90f525
Eric James <eric.james@yale.edu>
2021-04-14 15:33:43 -0400

    require bootstrap-sprockets not bootstrap which causes modal to not work

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,7 +14,7 @@
 //= require jquery_ujs
 //= require turbolinks//
 //= require bootstrap/carousel
-//= require bootstrap
+//= require bootstrap-sprockets
 // Required by Blacklight
 //= require blacklight/blacklight
 //= require jquery.fancybox.min
