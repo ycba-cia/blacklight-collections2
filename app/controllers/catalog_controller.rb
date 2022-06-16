99ae4c46c3bfee9a7ae6b68c02166d02c70278c4
Eric James <eric.james@yale.edu>
2022-06-16 11:37:46 -0400

    Image Use facet label, cc/rightstatement icon

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -110,7 +110,7 @@ class CatalogController < ApplicationController
     config.add_facet_field 'author_ss', label: 'Creator', :limit => 20
     config.add_facet_field 'earliestDate_is', :label => 'Date', single: true,range: { segments: false }
     config.add_facet_field 'detailed_onview_ss', :label => 'On-site Access'
-    config.add_facet_field 'rights_ss', helper_method: 'rights_helper',label: 'Rights'
+    config.add_facet_field 'rights_ss', helper_method: 'rights_helper',label: 'Image Use'
     config.add_facet_field 'has_image_ss', helper_method: 'capitalize', :label => 'Image Available'
     config.add_facet_field 'type_ss', :label => 'Classification', :limit => 20
     config.add_facet_field 'author_gender_ss', :label => 'Creator Gender'
