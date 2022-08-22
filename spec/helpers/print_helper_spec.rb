810ce00af2beaf8a30cc8f32feb9618a2af00850
Eric James <eric.james@yale.edu>
2022-08-22 15:55:08 -0400

    print_helper rspecs complate

--- spec/helpers/print_helper_spec.rb
+++ spec/helpers/print_helper_spec.rb
@@ -45,9 +45,40 @@ describe PrintHelper do
   end
   describe "#get_solr_doc" do
     it "gets tested" do
-      mock = double("request")
       solrdoc = helper.get_solr_doc(@id1,"https://","collections-test.britishart.yale.edu")
       expect(solrdoc["callnumber_ss"][0]).to be == "B1977.14.77"
+    end
+  end
+
+  describe "#print_fields" do
+    it "gets tested" do
+      @document = JSON.parse(File.read("spec/fixtures/dort.json"))
+      printed_field1 = helper.print_fields("Title:","title_ss")
+      expect(printed_field1).to be == "<dt style=\"overflow: hidden;\">Title:</dt><dd>Dort or Dordrecht: The Dort Packet-Boat from Rotterdam Becalmed</dd>"
+      printed_field2 = helper.print_fields("Title:","empty_ss")
+      expect(printed_field2).to be == ""
+      printed_newline_field = helper.print_newline_fields("Creator:","author_ss")
+      expect(printed_newline_field).to be == "<dt>Creator:</dt><dd><span>Joseph Mallord William Turner, 1775–1851, British</span></p></dd>"
+      printed_default_empty1 = helper.print_fields_default_empty("Copyright Status:","ort_ss","Unknown")
+      expect(printed_default_empty1).to be == "<dt>Copyright Status:</dt><dd>Public Domain</dd>"
+      printed_default_empty2 = helper.print_fields_default_empty("Copyright Status:","empty_ss","Unknown")
+      expect(printed_default_empty2).to be == "<dt>Copyright Status:</dt><dd>Unknown</dd>"
+      printed_sep_fields1 = helper.print_sep_fields("Subject Terms:","topic_ss")
+      expect(printed_sep_fields1).to be == "<dt>Subject Terms:</dt><dd>marine art | crowd | sea | church | costume | jars | ship | ships | market (event) | women | sunlight | men | cityscape | flags | seascape | rowboats | fruit | rubbish | reflection | chromaticity | river | city | flotsam | vegetables</dd>"
+      printed_sep_fields2 = helper.print_sep_fields("Subject Terms:","empty_ss")
+      expect(printed_sep_fields2).to be == ""
+
+      @document = JSON.parse(File.read("spec/fixtures/helmingham.json"))
+      printed_field = helper.print_fields("Title:","title_ss")
+      expect(printed_field).to be == "<dt style=\"overflow: hidden;\">Title:</dt><dd>Helmingham herbal and bestiary.</dd>"
+      printed_newline_field = helper.print_newline_fields("Creator:","author_ss")
+      expect(printed_newline_field).to be == ""
+      printed_default_empty1 = helper.print_fields_default_empty("Copyright Status:","ort_ss","Unknown")
+      expect(printed_default_empty1).to be == "<dt>Copyright Status:</dt><dd>Copyright Information</dd>"
+      printed_default_empty2 = helper.print_fields_default_empty("Copyright Status:","empty_ss","Unknown")
+      expect(printed_default_empty2).to be == "<dt>Copyright Status:</dt><dd>Unknown</dd>"
+      printed_sep_fields = helper.print_sep_fields("Subject Terms:","topic_ss")
+      expect(printed_sep_fields).to be == "<dt>Subject Terms:</dt><dd>Tollemache family. | Bodleian Library. Manuscript. Ashmole 1504. | Helmingham Hall. | Agnus castus (Middle English herbal) | Hortus sanitatis. | Animals, Mythical, in art. | Animals in art. | Animals -- Folklore. | Plants in art. | Decoration and ornament -- England. | Interior decoration -- England.</dd>"
     end
   end
 
